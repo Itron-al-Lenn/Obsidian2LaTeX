453765c1ec53183781e72eaf774e17e015f84740
Vivien <jollertime@gmail.com>
2023-10-26 16:32:04 +0000

    added option to edit the output directory and switched to pathlib for the pathes

--- Obsidian2LaTeX_helper.py
+++ Obsidian2LaTeX_helper.py
@@ -3,10 +3,12 @@ import shutil
 import subprocess
 import tempfile
 import time
+from pathlib import Path
 
 
-def convert_MD2TeX(in_path, name):
-    output_path = "output/.TeX"
+def convert_MD2TeX(in_path, name, Output_dir="output"):
+    output_dir = Path(Output_dir)
+    output_path = output_dir / ".TeX"
 
     with open(in_path) as MD:
         MDtext = MD.read()
@@ -14,35 +16,36 @@ def convert_MD2TeX(in_path, name):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    file_path = os.path.join(output_path, name + ".tex")
+    file_path = output_path / (name + ".tex")
 
     with open(file_path, "w") as file:
         file.write(MDtext)
 
 
-def bake_TeX(name):
+def bake_TeX(name, Output_dir="output"):
+    output_dir = Path(Output_dir)
     try:
         # Create a temporary directory and get its path
-        temp_dir = tempfile.mkdtemp()
+        temp_dir = Path(tempfile.mkdtemp())
 
         # Copy the generated .tex file in the temporary directory.
         shutil.copy(
-            "output/.TeX/" + name + ".tex",
-            temp_dir + "/" + name + ".tex",
+            output_dir / ".TeX/" / (name + ".tex"),
+            temp_dir / (name + ".tex"),
         )
 
         # Run pdflatex with the file in the temporary directory
         # and make sure that pdflatex is finished before the program continuous
-        subprocess.check_call(["pdflatex", temp_dir + "/" + name + ".tex"], cwd=temp_dir)
+        subprocess.check_call(["pdflatex", str(temp_dir) + "/" + name + ".tex"], cwd=temp_dir)
         time.sleep(1)
 
         # Create the output directory for the .pdf if it doesn't exists
 
-        if not os.path.exists("output/.pdf/"):
-            os.mkdir("output/.pdf/")
+        if not os.path.exists(output_dir / ".pdf/"):
+            os.mkdir(output_dir / ".pdf/")
 
         # Move the new .pdf file in the output/.pdf/ directory
-        shutil.move(temp_dir + "/" + name + ".pdf", "output/.pdf/" + name + ".pdf")
+        shutil.move(temp_dir / (name + ".pdf"), output_dir / ".pdf" / (name + ".pdf"))
 
         # Delete the temporary folder and its contents
         shutil.rmtree(temp_dir)
