8b4013265d0a70849bffae5c5c121b5f11cec32f
Vivien <jollertime@gmail.com>
2023-10-26 15:22:44 +0000

    update

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from file_helper import get_file
 from Obsidian2LaTeX_helper import bake_TeX, convert_MD2TeX
 
 
-def main():
-    name = "Test"
+def main(mddir, name):
     get_file()
-    convert_MD2TeX("test.md", name)
+    convert_MD2TeX(mddir, name)
     bake_TeX(name)
 
 
 if __name__ == "__main__":
-    main()
+    main("test.md", "test")
